4df0ed123121e2f3685efe52deed2013d0123938
Nathan Rabeau <nathan.rabeau@gmail.com>
2023-09-04 07:53:05 +0200

    kk

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,19 +1,11 @@
 const Turbolinks = require('turbolinks');
 const drawer = require('./drawer');
-const darkMode = require('./dark-mode');
 
 // Initialize Turbolinks
 Turbolinks.start();
 
 // Initialize mobile nav drawer
 drawer();
-
-// Initialize dark mode toggle
-const { enableThemeSwitch } = document.documentElement.dataset;
-
-if (enableThemeSwitch) {
-  darkMode();
-}
 
 // Handle Netlify Identity Login
 if (window.netlifyIdentity) {
