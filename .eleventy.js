d4e6dbbfbccefdcc344451a540124fed7219f6ac
Nathan Rabeau <nathan.rabeau@gmail.com>
2023-09-12 14:48:54 +0200

    fff

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/favicon.ico');
   eleventyConfig.addPassthroughCopy('./src/admin');
   eleventyConfig.addPassthroughCopy('./src/assets/img');
-  eleventyConfig.addPassthroughCopy('./src/assets/fonts');
 
   // Allow Turbolinks to work in development mode
   eleventyConfig.setBrowserSyncConfig(browserSyncConfig);
